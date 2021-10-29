422f0456bcddd010de902547e454e824ce07ab7d
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2021-10-29 22:24:01 +0200

    upgrade test_rest_api

--- task2/test.py
+++ task2/test.py
@@ -10,3 +10,10 @@ class Test:
         response = requests.get(config.API_BASE_URL + 'resources/',
                                 headers=main.headers, params={'path': 'new_folder'})
         assert response.status_code == 200
+
+    def test_rest_api_negative(self):
+        main.create_folder()
+        response = requests.get(config.API_BASE_URL + 'resources/',
+                                headers=main.headers, params={'path': 'other_folder'})
+        assert response.status_code == 404
+
